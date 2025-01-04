cac8ea81aeefab959c0220e4198ba8051e16a68c
quinn <store177447@gmail.com>
2025-01-04 16:09:30 +0800

    finish

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
       name='efftime',
-      version='1.0.3',
+      version='1.0.5',
       author='BreezeSun',
       description='A check Program run efficiency.',
       packages=find_packages(),

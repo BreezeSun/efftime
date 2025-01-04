4c3164bc7c0c90db48e71cad1a85a11d7e4e935c
quinn <store177447@gmail.com>
2025-01-04 16:31:25 +0800

    finish

--- efftime/efftime.py
+++ efftime/efftime.py
@@ -2,14 +2,24 @@ import time
 
 global_efftime = time.time()
 step_efftime = time.time()
+stop_flag = False
+
+def stop():
+    global stop_flag
+    stop_flag = True
+
 
 def start():
-    global global_efftime, step_efftime
+    global global_efftime, step_efftime, stop_flag
+    if stop_flag:
+        return
     global_efftime = time.time()
     step_efftime = time.time()
 
 def end(step=''):
-    global global_efftime, step_efftime
+    global global_efftime, step_efftime, stop_flag
+    if stop_flag:
+        return
     if step == '':
         step_efftime = time.time()
         print(f"Run time:", round(time.time() - global_efftime, 3))
